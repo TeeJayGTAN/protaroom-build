a099b21d61793e9d8c9576d7851464d027d0cb3a
unknown <oniretemisan@gmail.com>
2022-04-09 13:47:26 +0100

    First upload sanity tailwind and next

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Protaroom</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      <h1>Protaroom</h1>
+    </div>
+
+  )
+}
+
+export default Home;
