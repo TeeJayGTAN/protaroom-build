2c00f8bc8a69c3b18d45bd23c76f2df92b687437
unknown <oniretemisan@gmail.com>
2022-04-12 09:47:26 +0100

    still fixing

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import { useState } from 'react'
 import HamburgerMenu from './HamburgerMenu'
-import logo from '../public/assets/images/protaroom.PNG'
+// import logo from '../public/assets/images/protaroom.PNG'
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
