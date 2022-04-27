675f08da106512c85bef94cc2246ade2693bf2bf
unknown <oniretemisan@gmail.com>
2022-04-27 10:45:05 +0100

    fixed project id

--- sanity.js
+++ sanity.js
@@ -11,7 +11,7 @@ export const config = {
    * https://nextjs.org/docs/basic-features/environment-variables
    **/
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '7ru5ljyu',
   apiVersion: '2022-04-25',
 
   useCdn: process.env.NODE_ENV === 'production',
